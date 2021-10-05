defbb4bf8ff6835d9f06996dfd081a04d0a78b18
alex-jhnsn <alextheviking@hotmail.co.uk>
2021-10-05 21:38:46 +0100

    Add logging to deploy-command.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,9 +3,13 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 
+console.log("Deploying commands to server...");
+
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 const token = process.env.TOKEN;
+
+console.log(`ClientId: ${clientId}, GuildId: ${guildId}`);
 
 const commands = [
 	new SlashCommandBuilder().setName('info').setDescription('Returns info about me, Rando Sumo.'),
@@ -13,8 +17,14 @@ const commands = [
 ]
 	.map(command => command.toJSON());
 
+console.log("Commands built successfully")
+
 const rest = new REST({ version: '9' }).setToken(token);
+
+console.log("REST configured");
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
+
+console.log("Deployed commands to server successsfully");
